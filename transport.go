5e24c11dfdcf64836dcb55f3e0855789ba0d9f69
lyekumchew <lyekumchew@gmail.com>
2024-02-08 19:48:50 +0800

    fix: update tls fingerprint

--- transport.go
+++ transport.go
@@ -27,77 +27,11 @@ func headers(req *http.Request) {
 }
 
 func (*uTransport) newSpec() *tls.ClientHelloSpec {
-	return &tls.ClientHelloSpec{
-		CipherSuites: []uint16{
-			tls.GREASE_PLACEHOLDER,
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
-		CompressionMethods: []uint8{
-			tls.CompressionNone,
-		},
-		Extensions: []tls.TLSExtension{
-			&tls.UtlsGREASEExtension{},
-			&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
-				tls.PskModeDHE,
-			}},
-			&tls.SNIExtension{},
-			&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
-			&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
-				tls.ECDSAWithP256AndSHA256,
-				tls.PSSWithSHA256,
-				tls.PKCS1WithSHA256,
-				tls.ECDSAWithP384AndSHA384,
-				tls.PSSWithSHA384,
-				tls.PKCS1WithSHA384,
-				tls.PSSWithSHA512,
-				tls.PKCS1WithSHA512,
-			}},
-			&tls.SupportedVersionsExtension{Versions: []uint16{
-				tls.GREASE_PLACEHOLDER,
-				tls.VersionTLS13,
-				tls.VersionTLS12,
-			}},
-			&tls.ALPSExtension{SupportedProtocols: []string{"h2"}},
-			&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
-				tls.CurveID(tls.GREASE_PLACEHOLDER),
-				tls.X25519,
-				tls.CurveP256,
-				tls.CurveP384,
-			}},
-			&tls.UtlsExtendedMasterSecretExtension{},
-
-			&tls.SessionTicketExtension{},
-			&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
-				tls.CertCompressionBrotli,
-			}},
-			&tls.SCTExtension{},
-			&tls.StatusRequestExtension{},
-			&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
-				{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
-				{Group: tls.X25519},
-			}},
-			&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateOnceAsClient},
-			&tls.SupportedPointsExtension{SupportedPoints: []byte{
-				tls.PointFormatUncompressed,
-			}},
-			&tls.UtlsGREASEExtension{},
-			&tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
-		},
+	spec, err := tls.UTLSIdToSpec(tls.HelloChrome_120)
+	if err != nil {
+		panic(err)
 	}
+	return &spec
 }
 
 func (u *uTransport) RoundTrip(req *http.Request) (*http.Response, error) {
